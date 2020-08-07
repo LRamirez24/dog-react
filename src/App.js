afd41ccd6730c658632f6a42600f79a4bfb48403
luis Ramirez <l.r.32@hotmail.com>
2020-08-06 22:54:09 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+import { Router, Link } from "@reach/router";
+import SearchParams from "./SearchParams";
+import Details from "./details";
+import ThemeContext from "./themeContext";
 
-function App() {
+const App = () => {
+  const themeHook = useState("darkblue");
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeContext.Provider value={themeHook}>
+      <div>
+        <header>
+          <Link to="/">Adopt Me!</Link>
+        </header>
+        <Router>
+          <SearchParams path="/" />
+          <Details path="/details/:id" />
+        </Router>
+      </div>
+    </ThemeContext.Provider>
   );
-}
+};
 
 export default App;
+
